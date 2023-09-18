c37041ab16c6a86dd7698d909f8b990c2887ecf4
Nadine Lewit <nadine.lewit@gmail.com>
2023-09-18 19:22:31 -0300

    avances nadu

---
+++ tp_productos/src/components/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id: 1, imgUrl: "src/components/img/imagen.jpg"
+    },
+    {
+        id: 2, imgUrl: "src/components/img/imagen.jpg"
+    },
+    {
+        id: 3, imgUrl: "src/components/img/imagen.jpg"
+    },
+    {
+        id: 4, imgUrl: "src/components/img/imagen.jpg"
+    },
+    {
+        id: 5, imgUrl: "src/components/img/imagen.jpg"
+    },
+    {
+        id: 6, imgUrl: "tp_productos/src/components/img/imagen.jpg"
+    }
+];
